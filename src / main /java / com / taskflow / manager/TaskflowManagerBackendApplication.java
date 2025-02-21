940d24e1f4e8e8e9b55f796025c4707a2e2a29de
chandan-kumar-singh <chandankumar8730.ck@gmail.com>
2025-02-21 12:45:31 +0530

    Add files via upload

---
+++ src / main /java / com / taskflow / manager/TaskflowManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskflow.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowManagerBackendApplication.class, args);
+	}
+
+}
